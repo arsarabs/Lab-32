b3512e26369b6eea15ff80e3b358a5f18dad5b11
Your Name <your-email@example.com>
2024-11-15 03:35:53 -0800

    wip

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,27 @@
 //Anthony Sarabia - Lab 28 
+
+#include <iostream>
+#include <deque>
+#include <cstdlib>
+#include <ctime>
+#include "Car.h"
+
+using namespace std;
+
+const int QUEUE_SIZE = 2;
+const int PAY_PROB = 55;
+const int JOIN_PROB = 45;
+
+
+int randomNumber();
+int printDivider();
+void seedRandom();
+void intializeQueue(deque<Car>& tollQueue);
+void simulateCycle(deque<Car>& tollQueue, int cycle);
+
+
+int main() {
+
+
+	return 0;
+}
